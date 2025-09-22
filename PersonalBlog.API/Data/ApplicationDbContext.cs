e9c88afa561e94c5e7a9054a81b17df40b17d838
namThienHoang <namlv@thienhoang.com.vn>
2025-09-22 14:07:28 +0700

    update context

--- PersonalBlog.API/Data/ApplicationDbContext.cs
+++ PersonalBlog.API/Data/ApplicationDbContext.cs
@@ -34,9 +34,9 @@ namespace PersonalBlog.API.Data
             // Seed Categories
             var categories = new List<Category>
             {
-                new() { Id = Guid.Parse("f1234567-8901-2345-6789-012345678901"), Name = "Technology", UrlHandle = "technology" },
-                new() { Id = Guid.Parse("f1234567-8901-2345-6789-012345678902"), Name = "Programming", UrlHandle = "programming" },
-                new() { Id = Guid.Parse("f1234567-8901-2345-6789-012345678903"), Name = "Web Development", UrlHandle = "web-development" }
+                new() { Id =  Guid.NewGuid(), Name = "Technology", UrlHandle = "technology" },
+                new() { Id =  Guid.NewGuid(), Name = "Programming", UrlHandle = "programming" },
+                new() { Id = Guid.NewGuid(), Name = "Web Development", UrlHandle = "web-development" }
             };
 
             modelBuilder.Entity<Category>().HasData(categories);
@@ -44,11 +44,11 @@ namespace PersonalBlog.API.Data
             // Seed Tags
             var tags = new List<Tag>
             {
-                new() { Id = Guid.Parse("t1234567-8901-2345-6789-012345678901"), Name = "C#", UrlHandle = "csharp" },
-                new() { Id = Guid.Parse("t1234567-8901-2345-6789-012345678902"), Name = ".NET", UrlHandle = "dotnet" },
-                new() { Id = Guid.Parse("t1234567-8901-2345-6789-012345678903"), Name = "Entity Framework", UrlHandle = "entity-framework" },
-                new() { Id = Guid.Parse("t1234567-8901-2345-6789-012345678904"), Name = "API", UrlHandle = "api" },
-                new() { Id = Guid.Parse("t1234567-8901-2345-6789-012345678905"), Name = "React", UrlHandle = "react" }
+                new() { Id =  Guid.NewGuid(), Name = "C#", UrlHandle = "csharp" },
+                new() { Id =  Guid.NewGuid(), Name = ".NET", UrlHandle = "dotnet" },
+                new() { Id =  Guid.NewGuid(), Name = "Entity Framework", UrlHandle = "entity-framework" },
+                new() { Id =  Guid.NewGuid(), Name = "API", UrlHandle = "api" },
+                new() { Id =  Guid.NewGuid(), Name = "React", UrlHandle = "react" }
             };
 
             modelBuilder.Entity<Tag>().HasData(tags);
